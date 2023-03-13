34a4c31d7b60eb720d4f08b6177b2ad248608128
veerviren <virenvariya7@gmail.com>
2023-03-13 16:29:58 +0530

    Api data reflect on api config

---
+++ app/src/main/java/com/example/scorecards/ThisApp.kt
@@ -0,0 +1,7 @@
+package com.example.scorecards
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ThisApp : Application()
---
+++ app/src/main/java/com/example/scorecards/ui/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.scorecards.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.scorecards.R
+import com.example.scorecards.databinding.CardDesignBinding
+import com.example.scorecards.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+    private lateinit var binding: CardDesignBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = CardDesignBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.userState.collect { response ->
+                    Log.d("MainActivity", response.toString())
+                    when (response) {
+                        is Resource.Error -> {
+                            Toast.makeText(
+                                this@MainActivity ,
+                                response.message!!,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        is Resource.Loading -> {}
+                        is Resource.Success -> {
+                            val user = response.data!!
+                            binding.apply {
+                                userName.text = user.handle
+                                maxRank.text = user.maxRank
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        viewModel.getUser("direction_")
+    }
+}
