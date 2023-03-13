12767638b580e5faf4be81c5c964699aaa7e9919
veerviren <virenvariya7@gmail.com>
2023-03-14 00:31:04 +0530

    logic added for handel colors

--- app/src/main/java/com/example/scorecards/ui/MainActivity.kt
+++ app/src/main/java/com/example/scorecards/ui/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.scorecards.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -46,13 +48,27 @@ class MainActivity : AppCompatActivity() {
                                 currentRating.text = getString(R.string.currentRatingNum, user.rating)
                                 maxRating.text = getString(R.string.maxRating, user.maxRating)
                                 queSolved.text = getString(R.string.noOfProblems, user.totalQuestionsSolved)
-//                                userImage.imageAlpha = user.avatar
+
+                                // logic for color change in handle
+                                when {
+                                    user.rating <= 1200 -> userName.setTextColor(Color.parseColor("#CCCCCC")) // Newbie
+                                    user.rating <= 1400 -> userName.setTextColor(Color.parseColor("#77FF77")) // Pupil
+                                    user.rating <= 1600 -> userName.setTextColor(Color.parseColor("#77DDBB")) // Specialist
+                                    user.rating <= 1900 -> userName.setTextColor(Color.parseColor("#AAAAFF")) // Expert
+                                    user.rating <= 2100 -> userName.setTextColor(Color.parseColor("#ff88ff")) // Candidate Master
+                                    user.rating <= 2300 -> userName.setTextColor(Color.parseColor("#FFCC88")) // Master
+                                    user.rating <= 2400 -> userName.setTextColor(Color.parseColor("#FFBB55")) // International Master
+                                    user.rating <= 2600 -> userName.setTextColor(Color.parseColor("#FF7777")) // Grandmaster
+                                    user.rating <= 3000 -> userName.setTextColor(Color.parseColor("#FF3333")) // International Grandmaster
+                                    user.rating <= 4000 -> userName.setTextColor(Color.parseColor("#EDC240")) // Legendary Grandmaster
+                                    else -> userName.setTextColor(Color.parseColor("#000000")) // black
+                                }
                             }
                         }
                     }
                 }
             }
         }
-        viewModel.getUser("direction_")
+        viewModel.getUser("Bakry")
     }
 }
