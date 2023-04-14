4e8c86b28e7a540c7bb98f615c6a9c85ce613aff
viren <virenviren7@gmail.com>
2023-04-14 19:59:02 +0530

    Added button : for adding friend handle name

--- app/src/main/java/com/example/scorecards/ui/DetailCard.kt
+++ app/src/main/java/com/example/scorecards/ui/DetailCard.kt
@@ -2,11 +2,22 @@ package com.example.scorecards.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import com.example.scorecards.R
 
 class DetailCard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_card)
+
+        val btn: Button = findViewById(R.id.add_friend_handle)
+
+        btn.setOnClickListener {
+            Log.d("DetailCard", "Button clicked")
+            Toast.makeText(this, "btn clicked", Toast.LENGTH_SHORT).show()
+            Log.d("DetailCard", "Toast shown")
+        }
     }
 }
