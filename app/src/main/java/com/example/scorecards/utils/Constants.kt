85d328746a798264066a52be1a2a05f53cd4f820
viren-Caw-Studios <viren.v@cawstudios.com>
2023-07-15 12:11:05 +0530

    FIX: base url change

--- app/src/main/java/com/example/scorecards/utils/Constants.kt
+++ app/src/main/java/com/example/scorecards/utils/Constants.kt
@@ -2,6 +2,7 @@ package com.example.scorecards.utils
 
 class Constants {
      companion object {
-         const val CODEFORCES_API_URL = "https://www.codeforces.com/"
+         const val CODEFORCES_API_URL = "https://codeforces.com/"
+         const val CODEFORCES_API_URL1 = "https://www.codeforces.com"
      }
 }
